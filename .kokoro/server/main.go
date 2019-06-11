9c7a2771c8215d2428ac2fafcf9c44cb5f96d7e2
Ana Clarissa Miranda Pena <cmirandap@google.com>
2019-06-11 16:43:20 -0600

    resolved changes

--- .kokoro/server/main.go
+++ .kokoro/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net"
 
@@ -18,27 +17,23 @@ const (
 type server struct{}
 
 func (s *server) Read(ctx context.Context, in *pb.ObjectRead) (*pb.Completed, error) {
-	storage.Read("a", in.BucketName, in.ObjectName)
-	fmt.Printf("%s\n", "Reading")
-	return &pb.Completed{Number: 2, Error: ""}, nil
+	storage.Read(in.BucketName, in.ObjectName)
+	return &pb.Completed{}, nil
 }
 
 func (s *server) Write(ctx context.Context, in *pb.ObjectWrite) (*pb.Completed, error) {
-	storage.Write("a", in.BucketName, in.ObjectName, in.Destination)
-	fmt.Printf("%s\n", "Writting")
-	return &pb.Completed{Number: 1, Error: ""}, nil
+	storage.Write(in.BucketName, in.ObjectName, in.Destination)
+	return &pb.Completed{}, nil
 }
 
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-		fmt.Printf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterStorageServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-		fmt.Printf("failed to serve: %v", err)
 	}
 }
--- storage/storage.go
+++ storage/storage.go
@@ -4,10 +4,10 @@ import (
 	"time"
 )
 
-func Read(foo, bucketName string, objectName string) {
+func Read(bucketName string, objectName string) {
 	time.Sleep(2000 * time.Millisecond)
 }
 
-func Write(foo, bucketName string, objectName string, destination string) {
+func Write(bucketName string, objectName string, destination string) {
 	time.Sleep(5000 * time.Millisecond)
 }
