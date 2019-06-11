ab20cf8e8d9d4c155188c1c5977c4a68af67a5d3
Ana Clarissa Miranda Pena <cmirandap@google.com>
2019-06-11 15:21:28 -0600

    vet

--- .kokoro/client/main.go
+++ .kokoro/client/main.go
@@ -11,10 +11,10 @@ import (
 )
 
 const (
-	address     = "localhost:50051"
+	address = "localhost:50051"
 )
 
-func main(){
+func main() {
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -24,13 +24,13 @@ func main(){
 	c := pb.NewStorageClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	r, err := c.Read(ctx, &pb.ObjectRead{BucketName:"bucket",ObjectName:"object"})
+	r, err := c.Read(ctx, &pb.ObjectRead{BucketName: "bucket", ObjectName: "object"})
 	if err != nil {
 		log.Fatalf("could not read: %v", err)
 		fmt.Printf("could not read: %v \n", err)
 	}
 	fmt.Printf("Read: %v \n", r.Number)
-	w, err := c.Write(ctx, &pb.ObjectWrite{BucketName:"bucket",ObjectName:"object",Destination:"destination"})
+	w, err := c.Write(ctx, &pb.ObjectWrite{BucketName: "bucket", ObjectName: "object", Destination: "destination"})
 	if err != nil {
 		log.Fatalf("could not write: %v", err)
 		fmt.Printf("could not write: %v \n", err)
--- storage/storage.go
+++ storage/storage.go
@@ -8,6 +8,6 @@ func Read(foo, bucketName string, objectName string) {
 	time.Sleep(2000 * time.Millisecond)
 }
 
-func Write(foo, bucketName string, objectName string, destination string){
+func Write(foo, bucketName string, objectName string, destination string) {
 	time.Sleep(5000 * time.Millisecond)
 }
