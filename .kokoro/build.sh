d0ffc6ecdd5130e2bb0913c4b646590cb9279f49
Jean de Klerk <deklerk@google.com>
2019-06-17 15:32:24 -0600

    check kokoro blaze dir

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+set -ex
+
 echo "Hello World"
+
+env
+
+echo $KOKORO_BLAZE_DIR
+ls $KOKORO_BLAZE_DIR
+ls $KOKORO_BLAZE_DIR/build_ls
+ls $KOKORO_BLAZE_DIR/build_ls/blaze-bin
+ls $KOKORO_BLAZE_DIR/build_ls/blaze-bin/devrel
+ls $KOKORO_BLAZE_DIR/build_ls/blaze-bin/devrel/cloud
+ls $KOKORO_BLAZE_DIR/build_ls/blaze-bin/devrel/cloud/client_libraries
+ls $KOKORO_BLAZE_DIR/build_ls/blaze-bin/devrel/cloud/client_libraries/benchmarking
+$KOKORO_BLAZE_DIR/build_ls/blaze-bin/devrel/cloud/client_libraries/benchmarking/ls
 
 pwd
